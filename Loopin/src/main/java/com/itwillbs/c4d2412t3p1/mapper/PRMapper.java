e9f28e5789bec5117c58dc53892d4b7da3306b96
waterkyeong <soo94.k@gmail.com>
2025-01-06 13:46:26 +0900

    [급여 관리] -웹 완성 -서버 연결 완성 -테이블 조인 여부 보류

---
+++ Loopin/src/main/java/com/itwillbs/c4d2412t3p1/mapper/PRMapper.java
@@ -0,0 +1,18 @@
+package com.itwillbs.c4d2412t3p1.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PRMapper {
+
+	List<Map<String, Object>> selectpradmin();
+
+	List<Map<String, Object>> selectpradminfirstmodal(Long pr_id);
+
+	List<Map<String, Object>> selectpradminfirstmodal2(@Param("prdetail_id") Long prdetail_id);
+	
+}
