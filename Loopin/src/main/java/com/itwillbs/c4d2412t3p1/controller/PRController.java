2cee4bb02d02eb4af5e4d6f084d608afd90bd85c
waterkyeong <soo94.k@gmail.com>
2025-01-13 09:10:19 +0900

    [급여 조회]

--- Loopin/src/main/java/com/itwillbs/c4d2412t3p1/controller/PRController.java
+++ Loopin/src/main/java/com/itwillbs/c4d2412t3p1/controller/PRController.java
@@ -1,10 +1,9 @@
 package com.itwillbs.c4d2412t3p1.controller;
-
 import java.util.List;
-import java.util.Map;
 
 import javax.script.ScriptException;
 
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +15,6 @@ import com.itwillbs.c4d2412t3p1.domain.PRDTO;
 import com.itwillbs.c4d2412t3p1.domain.PR_calculationMDTO;
 import com.itwillbs.c4d2412t3p1.entity.PRCode;
 import com.itwillbs.c4d2412t3p1.service.PRService;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 
@@ -26,32 +24,6 @@ import lombok.extern.java.Log;
 public class PRController {
 
 	private final PRService prS;
-	
-	@GetMapping("/prcal")
-	public String prcal() {
-		return "payroll/prcalcul";
-	}
-	
-	@PostMapping("/calculate")
-	@ResponseBody
-	public List<PRDTO> calsal(@RequestBody PR_calculationMDTO data) throws ScriptException{
-		log.info("클라이언트값!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+data.toString());
-		String BS = data.getBS();
-		log.info("BS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+BS);
-		String overworkingtime = data.getOverworkingtime();
-		String nightworkingtime = data.getNightworkingtime();
-		String weekendworkingtime = data.getWeekendworkingtime();
-		String holydayworkingtime = data.getHolydayworkingtime();
-		String leastannual = data.getLeastannual();
-		String bonus = data.getBonus();
-		List<PRDTO> list = prS.calculatingMachine(BS,overworkingtime,nightworkingtime,weekendworkingtime,holydayworkingtime,leastannual,bonus);
-//		for(PR_calculationMDTO d : data) {
-//		}
-//		log.info("클라이언트값!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+data.toString());
-//		List<PRDTO> list = prS.calculatingMachine(data);
-		log.info("가져온 값!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+list.toString());
-		return list;
-	}
 	
 	@GetMapping("/prcode")
 	public String prcode() {
@@ -89,7 +61,7 @@ public class PRController {
 		
 		return list;
 	}
-
+	
 	@GetMapping("/PRadminmadal2")
 	@ResponseBody
 	public List<Map<String,Object>> adminprmodal2(@RequestParam("prdetail_id") Long prdetail_id) {
@@ -98,4 +70,27 @@ public class PRController {
 		
 		return list;
 	}
+	
+	@GetMapping("/prcal")
+	public String prcal() {
+		return "payroll/prcalcul";
+	}
+	
+	@PostMapping("/calculate")
+	@ResponseBody
+	public List<PRDTO> calsal(@RequestBody PR_calculationMDTO data) throws ScriptException{
+		log.info("클라이언트값!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+data.toString());
+		String BS = data.getBS();
+		log.info("BS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+BS);
+		String overworkingtime = data.getOverworkingtime();
+		String nightworkingtime = data.getNightworkingtime();
+		String weekendworkingtime = data.getWeekendworkingtime();
+		String holydayworkingtime = data.getHolydayworkingtime();
+		String leastannual = data.getLeastannual();
+		String bonus = data.getBonus();
+		List<PRDTO> list = prS.calculatingMachine(BS,overworkingtime,nightworkingtime,weekendworkingtime,holydayworkingtime,leastannual,bonus);
+		log.info("가져온 값!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+list.toString());
+		return list;
+	}
+
 }
