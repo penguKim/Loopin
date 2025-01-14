bce7a1a30fd45c26de39f2adfd53cdeb4df68c9d
Huisu-Kim <141123301+penguKim@users.noreply.github.com>
2025-01-14 19:44:12 +0900

    [시큐리티 - @EnableMethodSecurity 추가]
    - @PreAuthorize를 사용한 메서드 레벨 권한 제어 가능

--- Loopin/src/main/java/com/itwillbs/c4d2412t3p1/config/SecurityConfig.java
+++ Loopin/src/main/java/com/itwillbs/c4d2412t3p1/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	private final MyUserDetailsService myUserDetailsService;
