800ab07ae4aef8b894f75a1f46195a24c5f65ea9
leeseungcheol <lsc2464@gmail.com>
2025-01-27 22:30:35 +0900

    [거래처관리] - 거래처 등록 관련 뷰 페이지 작업중..

---
+++ Loopin/src/main/java/com/itwillbs/c4d2412t3p1/controller/AccountController.java
@@ -0,0 +1,21 @@
+package com.itwillbs.c4d2412t3p1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+
+@RequiredArgsConstructor
+@Controller
+@Log
+public class AccountController {
+
+	
+    // 거래처 등록 페이지로 이동
+	@GetMapping("/account_list")
+	public String account_list(Model model) {
+		return "/account/account_list";
+	}
+}
