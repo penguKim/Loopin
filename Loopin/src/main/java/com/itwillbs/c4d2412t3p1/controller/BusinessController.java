2d1e4b4d5fb52c18fe79dd33759b6e94634e651b
leeseungcheol <lsc2464@gmail.com>
2025-01-29 19:50:45 +0900

    [발주관리] - 뷰페이지 작업중

---
+++ Loopin/src/main/java/com/itwillbs/c4d2412t3p1/controller/BusinessController.java
@@ -0,0 +1,24 @@
+package com.itwillbs.c4d2412t3p1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+
+@RequiredArgsConstructor
+@Controller
+@Log
+public class BusinessController {
+
+	// 발주관리 페이지로 이동
+	@GetMapping("/order_list")
+	public String order_list() {
+		
+		return "/business/order_list";
+	}
+	
+	
+	
+}
