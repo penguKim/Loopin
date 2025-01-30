4964cb2c3f235150f39bca84ab058939a04957bc
penguKim <kpg8226@gmail.com>
2025-01-30 21:13:10 +0900

    [창고관리 - 로직 작업 완료]
    - 등록, 수정, 삭제 작업
    - 공통코드 alert, toast 로직 수정
    - 필터모듈 셀렉트는 값 안넘어가는 경우 수정

--- Loopin/src/main/java/com/itwillbs/c4d2412t3p1/mapper/PrimaryMapper.java
+++ Loopin/src/main/java/com/itwillbs/c4d2412t3p1/mapper/PrimaryMapper.java
@@ -1,8 +1,17 @@
 package com.itwillbs.c4d2412t3p1.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.itwillbs.c4d2412t3p1.domain.WarehouseDTO;
+import com.itwillbs.c4d2412t3p1.util.FilterRequest.WarehouseFilterRequest;
 
 @Mapper
 public interface PrimaryMapper {
+
+	// 창고 조회
+	List<WarehouseDTO> select_WAREHOUSE_list(@Param("filter") WarehouseFilterRequest filter);
 	
 }
