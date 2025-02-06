0c6c90a869173e4e2261dbac08042c942285ea3b
waterkyeong <soo94.k@gmail.com>
2025-02-06 12:42:50 +0900

    [BOM] -테이블 생성

---
+++ Loopin/src/main/java/com/itwillbs/c4d2412t3p1/entity/Bom.java
@@ -0,0 +1,28 @@
+package com.itwillbs.c4d2412t3p1.entity;
+
+import groovy.transform.ToString;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@ToString
+@Table(name = "BOM")
+public class Bom {
+
+	@Id
+	@Column(name = "product_cd")
+	private String product_cd;
+	
+	@Id
+	@Column(name = "bom_cd")
+	private String bom_cd;
+	
+	@Column(name = "bom_am")
+	private String bom_am;
+	
+}
---
+++ Loopin/src/main/java/com/itwillbs/c4d2412t3p1/entity/BomProcess.java
@@ -0,0 +1,56 @@
+package com.itwillbs.c4d2412t3p1.entity;
+
+import java.security.Timestamp;
+
+import groovy.transform.ToString;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@ToString
+@Table(name = "BOMPROCESS")
+public class BomProcess {
+
+	@Id
+	@Column(name = "product_cd")
+	private String product_cd;
+	
+	@Id
+	@Column(name = "process_cd")
+	private String process_cd;
+	
+	@Id
+	@Column(name = "bomprocess_cd")
+	private String bomprocess_cd;
+	
+	@Id
+	@Column(name = "bomprocess_ap")
+	private String bomprocess_ap;
+	
+	@Column(name = "bomprocess_rt")
+	private String bomprocess_rt;
+	
+	@Column(name = "bomprocess_ra")
+	private String bomprocess_ra;
+	
+	@Column(name = "bomprocess_er")
+	private String bomprocess_er;
+	
+	@Column(name = "bomprocess_wr")
+	private String bomprocess_wr;
+	
+	@Column(name = "bomprocess_wd")
+	private Timestamp bomprocess_wd;
+	
+	@Column(name = "bomprocess_mf")
+	private String bomprocess_mf;
+	
+	@Column(name = "bomprocess_md")
+	private Timestamp bomprocess_md;
+	
+}
