060339c8d436a689520598464f02370272910f65
Huisu-Kim <141123301+penguKim@users.noreply.github.com>
2025-02-07 11:53:06 +0900

    [제품관리 - 저장 로직 수정]

--- Loopin/src/main/java/com/itwillbs/c4d2412t3p1/repository/MaterialRepository.java
+++ Loopin/src/main/java/com/itwillbs/c4d2412t3p1/repository/MaterialRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.itwillbs.c4d2412t3p1.entity.Material;
 import com.itwillbs.c4d2412t3p1.entity.MaterialPK;
+import com.itwillbs.c4d2412t3p1.entity.Product;
 
 
 @Repository
@@ -18,5 +20,8 @@ public interface MaterialRepository extends JpaRepository<Material, MaterialPK> 
     @Modifying
     @Query("DELETE FROM Material m WHERE m.material_cd IN :materialCodes")
 	void deleteByMaterialCd(@Param("materialCodes") List<String> materialCodes);
+
+    @Query("SELECT m.material_cd FROM Material m WHERE m.material_cd = :materialCd")
+    List<Material> findByMaterialCd(@Param("materialCd") String material_cd);
 
 }
